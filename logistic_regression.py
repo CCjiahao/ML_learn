95a5f53791962c57d259ac0db50d29a4dfda547b
CCjiahao <13462260020@163.com>
2023-04-16 11:46:27 +0800

    逻辑回归

---
+++ logistic_regression.py
@@ -0,0 +1,39 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    # 获取乳腺癌数据集
+    raw_data = load_breast_cancer()
+    
+    # 打印数据信息
+    print(f"数据维度: {raw_data.data.shape}")
+    print(f"维度信息: {raw_data.feature_names}")
+    print(f"预测信息: {raw_data.target_names}")
+    print(f"预测维度信息: {raw_data.target.shape}")
+
+    # 分割数据集(测试集30%)
+    xtrain, xtest, ytrain, ytest = train_test_split(raw_data.data, raw_data.target, test_size=0.3, random_state=1)
+
+    # 拟合模型 (C的含义是正则化强度的倒数)
+    model = LogisticRegression(penalty="l2", C=1)
+    model.fit(xtrain, ytrain)
+
+    # 打印模型参数
+    print("------------")
+    for name, w in zip(raw_data.feature_names, model.coef_[0]):
+        print(f"{name}: {w}")
+    print(f"b: {model.intercept_}")
+
+    # 计算模型精度
+    ypredict = model.predict(xtest)
+    acc = accuracy_score(ypredict, ytest)
+    print(f"acc: {acc}")
+
+
+if __name__ == '__main__':
+    main()
